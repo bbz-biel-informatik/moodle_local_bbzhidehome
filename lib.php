62bb0a3c3752db24ed73e56ecd1a1bb01e1487df
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-08-14 22:37:38 +0200

    Add PHP tag to lib.php

--- lib.php
+++ lib.php
@@ -1,3 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
 function local_bbzhidehome_extend_navigation(global_navigation $navigation) {
     if ($home = $navigation->find('home', global_navigation::TYPE_SETTING)) {
         $home->remove();
