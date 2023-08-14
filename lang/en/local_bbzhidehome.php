92f55b67f8446d67842eec101315b4b99d58edc9
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-08-14 22:42:43 +0200

    Add plugin name

---
+++ lang/en/local_bbzhidehome.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Local plugin "bbzhidehome" - Language pack
+ *
+ * @package    local_bbzhidehome
+ * @copyright  2023, Lukas Diener, <lukas.diener@bbz-cfp.ch>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'BBZ Hide Home';
--- lib.php
+++ lib.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Local plugin "bbzhidehome" - Lib
+ *
+ * @package    local_bbzhidehome
+ * @copyright  2023, Lukas Diener, <lukas.diener@bbz-cfp.ch>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-defined('MOODLE_INTERNAL') || die();
+ defined('MOODLE_INTERNAL') || die();
 
 function local_bbzhidehome_extend_navigation(global_navigation $navigation) {
     if ($home = $navigation->find('home', global_navigation::TYPE_SETTING)) {
